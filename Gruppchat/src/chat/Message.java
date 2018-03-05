47acaa6f2c291620daafa05251e6ddcba413fbb2
TedAhlberg <35498698+TedAhlberg@users.noreply.github.com>
2018-03-05 12:57:52 +0100

    Message dates

--- Gruppchat/src/chat/Message.java
+++ Gruppchat/src/chat/Message.java
@@ -1,20 +1,35 @@
 package chat;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-
-import javax.swing.Icon;
 
 public class Message {
 	private User sender;
-	private Date date;
-	
+	private String dateCreated;
+	private String dateReceived;
+
 	public Message(User sender) {
 		this.sender = sender;
 	}
-	
-	public void setDate(Date date) {
-		this.date = date;
+
+	public void setDateCreated() {
+		this.dateCreated = setDate();
+	}
+
+	public void setDateReceived() {
+		this.dateReceived = setDate();
+	}
+
+	private String setDate() {
+		Calendar cal = Calendar.getInstance();
+		return cal.get(Calendar.YEAR) + " " + (cal.get(Calendar.MONTH) + 1) + " " + cal.get(Calendar.DATE) + " "
+				+ cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
+	}
+
+	public String getDateCreated() {
+		return dateCreated;
+	}
+
+	public String getDateReceived() {
+		return dateReceived;
 	}
 }

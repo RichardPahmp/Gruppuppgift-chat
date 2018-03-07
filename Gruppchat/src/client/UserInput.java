037d64940dadcce27ceefc3fb2dafbdf2cd29bca
TedAhlberg <35498698+TedAhlberg@users.noreply.github.com>
2018-03-07 15:23:29 +0100

    UserList

--- Gruppchat/src/client/UserInput.java
+++ Gruppchat/src/client/UserInput.java
@@ -1,40 +1,60 @@
 package client;
 
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.event.KeyAdapter;
+import java.awt.Toolkit;
 
 import javax.swing.JButton;
-
-import chat.User;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class UserInput extends JFrame {
-	private JPanel mainPanel = new JPanel();
-	
+	private final int width = 300, height = 200;
+
 	private TextField nameField = new TextField("Ange ditt användarnamn...");
-	
-	private JTextField imageField = new JTextField();
-	private JFileChooser fileChooser = new JFileChooser();
+	private TextField imageField = new TextField("Sökväg till bild...");
 
-	private JButton JButton = new JButton("Klar");
-	private User user;
-	
-	public UserInput(ClientController controller) {
-		GridLayout gl1 = new GridLayout(1, 3);
+	private JFileChooser imageChooser = new JFileChooser();
+
+	private JButton fileButton = new JButton("Upload file");
+	private JButton doneButton = new JButton("Klar");
+
+	public UserInput() {
+
+		setTitle("New User");
+		setPreferredSize(new Dimension(width, height));
+		setResizable(false);
+
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Dimension screen = toolkit.getScreenSize();
+
+		setLocation((int) (screen.getWidth() / 2) - (width / 2), ((int) screen.getHeight() / 2) - (height / 2));
+
+		JPanel mainPanel = new JPanel();
+		GridLayout gl1 = new GridLayout(7, 1);
 		mainPanel.setLayout(gl1);
-		add(mainPanel);
-		mainPanel.add(nameField);
-	}
-	
-	private class KeyListener extends KeyAdapter{
-		
-	}
-	
-	public static void main(String[] args) {
 
+		mainPanel.add(new JLabel("Namn: "));
+		mainPanel.add(nameField);
+
+		mainPanel.add(new JLabel("Bild: "));
+		imageField.setFocusable(false);
+		mainPanel.add(imageField);
+
+		mainPanel.add(fileButton);
+
+		mainPanel.add(new JPanel());
+		mainPanel.add(doneButton);
+
+		add(mainPanel);
+	}
+
+	public static void main(String[] args) {
+		UserInput test = new UserInput();
+		test.pack();
+		test.setVisible(true);
 	}
 }

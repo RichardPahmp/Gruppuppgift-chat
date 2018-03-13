707953dc85c8b543bcaaff61397a6f0f1284ea8b
TedAhlberg <35498698+TedAhlberg@users.noreply.github.com>
2018-03-13 18:53:18 +0100

    kommentarer

--- Gruppchat/src/client/TextField.java
+++ Gruppchat/src/client/TextField.java
@@ -6,10 +6,20 @@ import java.awt.event.FocusEvent;
 
 import javax.swing.JTextField;
 
+/**
+ * Extends JTextField. Enables grayed out text when the TextField is empty or
+ * not focused.
+ */
 public class TextField extends JTextField {
-	private boolean selected = false;
 	private String text;
 
+	/**
+	 * Constructor for the TextField, initializes the component with grayed out
+	 * text.
+	 * 
+	 * @param text
+	 *            Default text
+	 */
 	public TextField(String text) {
 		super(text);
 		this.text = text;
@@ -17,26 +27,44 @@ public class TextField extends JTextField {
 		addFocusListener(new FocusListener());
 	}
 
+	/**
+	 * Returns the text if the user has entered any. Otherwise returns an empty
+	 * String.
+	 */
 	public String getText() {
-		if(getForeground() != Color.LIGHT_GRAY) {
-			return super.getText();			
-		} else return "";
+		if (getForeground() != Color.LIGHT_GRAY) {
+			return super.getText();
+		} else
+			return "";
 	}
-	
+
+	/**
+	 * Clears text in the TextField.
+	 */
 	public void clear() {
 		setText("");
 	}
 
+	/**
+	 * Private class to check if the component is focused by the user or not.
+	 */
 	private class FocusListener extends FocusAdapter {
+
+		/**
+		 * Clear the grayed out text from the TextField when the component gains focus.
+		 */
 		public void focusGained(FocusEvent e) {
 			if (getForeground() == Color.LIGHT_GRAY) {
-				setText("");
 				setForeground(Color.BLACK);
+				clear();
 			}
 		}
-
+		
+		/**
+		 * Sets the text back to the grayed out text if the TextField is empty
+		 */
 		public void focusLost(FocusEvent e) {
-			if (getForeground() != Color.BLACK || getText().length() == 0) {
+			if (getText().length() == 0) {
 				setForeground(Color.LIGHT_GRAY);
 				setText(text);
 			}
--- Gruppchat/src/client/UserList.java
+++ Gruppchat/src/client/UserList.java
@@ -18,12 +18,18 @@ import javax.swing.event.ListSelectionListener;
 
 import chat.User;
 
+/**
+ * Extends JPanel. A JList coustomized for the <code>User</code> class to render
+ * both picture and text in the same list cell.
+ */
 public class UserList extends JPanel {
 	private DefaultListModel<User> listModel = new DefaultListModel<>();
 	private ArrayList<User> activeUsers = new ArrayList<User>();
 	private JList<User> list;
 
-
+	/**
+	 * Requires a <code>MouseListener</code> for initiation.
+	 */
 	public UserList(MouseListener listener) {
 		list = new JList<>(listModel);
 		list.setCellRenderer(new UserRenderer());
@@ -32,14 +38,28 @@ public class UserList extends JPanel {
 		add(list);
 	}
 
+	/**
+	 * Adds a <code>User</code> to the list.
+	 * 
+	 * @param user
+	 */
 	public void addUser(User user) {
 		activeUsers.add(user);
 		setUsers(activeUsers);
 	}
 
+	/**
+	 * Clears the JList from Users.
+	 */
 	public void clearList() {
 		activeUsers.clear();
 	}
+
+	/**
+	 * Sets the JList with a ArrayList.
+	 * 
+	 * @param list
+	 */
 	public void setUsers(ArrayList<User> list) {
 		listModel.clear();
 		for (User u : list) {
@@ -49,28 +69,46 @@ public class UserList extends JPanel {
 		repaint();
 	}
 
+	/**
+	 * Return all the <code>User</code> classes contained in the JList.
+	 * 
+	 * @return ArrayList
+	 */
 	public ArrayList<User> getUsers() {
 		return activeUsers;
 	}
 
+	/**
+	 * Returns the highlighted <code>User</code> classes from the JList.
+	 * 
+	 * @return ArrayList
+	 */
 	public ArrayList<User> getSelectedUsers() {
 		ArrayList<User> selectedList = new ArrayList<User>();
 		int[] indexes = list.getSelectedIndices();
-		for(int n : indexes) {
+		for (int n : indexes) {
 			selectedList.add(list.getModel().getElementAt(n));
 		}
-//		for (int i = 0; i < listModel.size(); i++) {
-//			if (list.isSelectedIndex(i)) {
-//				selectedList.add(activeUsers.get(i));
-//			}
-//		}
+		// for (int i = 0; i < listModel.size(); i++) {
+		// if (list.isSelectedIndex(i)) {
+		// selectedList.add(activeUsers.get(i));
+		// }
+		// }
 		return selectedList;
 	}
 
+	/**
+	 * Adds a <code>ListSelectionListener</code>.
+	 * 
+	 * @param listener
+	 */
 	public void addListener(ListSelectionListener listener) {
 		list.addListSelectionListener(listener);
 	}
 
+	/**
+	 * Extends JLabel. Custom <code>ListCellRenderer</code> for the <code>User</code> class.
+	 */
 	private class UserRenderer extends JLabel implements ListCellRenderer<User> {
 		public UserRenderer() {
 			setOpaque(true);
@@ -79,7 +117,6 @@ public class UserList extends JPanel {
 		@Override
 		public Component getListCellRendererComponent(JList<? extends User> list, User user, int index,
 				boolean isSelected, boolean cellHasFocus) {
-
 			setIcon(user.getImage());
 			setText(user.getName() + " ");
 
